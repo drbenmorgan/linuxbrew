85b34fa5e75007c8125e9ab8dc6b4f50473a9f40
Ben Morgan <bmorgan.warwick@gmail.com>
2015-04-16 13:17:55 +0100

    cmake: Update python path for docs

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -46,7 +46,7 @@ class Cmake < Formula
 
   def install
     if build.with? "docs"
-      ENV.prepend_create_path "PYTHONPATH", buildpath+"sphinx/lib64/python2.6/site-packages"
+      ENV.prepend_create_path "PYTHONPATH", buildpath+"sphinx/lib/python2.7/site-packages"
       resources.each do |r|
         r.stage do
           system "python", *Language::Python.setup_install_args(buildpath/"sphinx")
